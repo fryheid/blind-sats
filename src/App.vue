3df03052a7933102a2d85b840d24242dd467ea86
Fryheid <fryheid@protonmail.com>
2022-12-07 23:32:47 +0100

    Created different views and a button to change the view

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,31 @@
       </div>
     </header>
     <main>
-      <p v-if="!wallets.length" class="mt-3 mx-3">No wallet found. Please create a wallet.</p>
-      <Wallets v-else :wallets="wallets" />
+      <template v-if="view === View.Home">
+        <p v-if="!wallets.length" class="mt-3 mx-3">
+          No wallet found. Please create a wallet.
+        </p>
+        <Wallets v-else :wallets="wallets" />
+
+        <div @click="view = View.AddWallet" class="bg-orange round text-lace absolute bottom-1vh right-1vh px-3 py-2">+</div>
+      </template>
+
+      <p v-else-if="view === View.AddWallet">Add wallet</p>
     </main>
   </body>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
-import Wallets from "./components/Wallets.vue"
+import Wallets from "./components/Wallets.vue";
+
+enum View {
+  Home,
+  AddWallet,
+  Wallet,
+}
+
+const view = ref(View.Home);
 
 const wallets = ref([
   {
