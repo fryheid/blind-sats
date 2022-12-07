64e94027f0f31188460621b36b803a8f3f4795ae
Fryheid <fryheid@protonmail.com>
2022-12-07 22:00:50 +0100

    Style mock data

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,42 @@
         <h1 class="text-lace">Blind sats</h1>
       </div>
     </header>
+    <main>
+      <ul class="list-unstyled">
+        <li
+          v-for="wallet in wallets"
+          :key="wallet.wallet_name"
+          class="flex justify-between border-bottom p-3"
+        >
+          <img
+            class="round h-60px minw-60px"
+            :src="`https://asats.io/anonsats/robohash/${wallet.wallet_name}`"
+          />
+          <div class="flex flex-column align-end">
+            <h2>{{ wallet.wallet_name }}</h2>
+            <p>{{ wallet.initial_balance }} sats</p>
+          </div>
+        </li>
+      </ul>
+    </main>
   </body>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+
+const wallets = ref([
+  {
+    wallet_key: "test-key",
+    wallet_name: "test-wallet",
+    lightning_address: "test@asats.io",
+    initial_balance: 0,
+  },
+  {
+    wallet_key: "test-key",
+    wallet_name: "test-wallet",
+    lightning_address: "test@asats.io",
+    initial_balance: 0,
+  },
+]);
+</script>
