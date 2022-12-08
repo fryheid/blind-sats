507403e4b2d63a651fe4ea440359896597399a88
Fryheid <fryheid@protonmail.com>
2022-12-08 21:55:53 +0100

    Add vue qr code package

--- src/components/Wallet.vue
+++ src/components/Wallet.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="h-full mx-3 pt-3 relative">
-    <h2 class="text-3xl font-black">{{ wallet.wallet_name }}</h2>
-    <p>{{ wallet.initial_balance }} sats</p>
+    <section>
+      <h2 class="text-3xl font-black">{{ wallet.wallet_name }}</h2>
+      <p>{{ wallet.initial_balance }} sats</p>
+    </section>
 
-    <h3 class="text-2xl font-bold">Receive</h3>
-    <p>{{ wallet.lightning_address }}</p>
+    <section class="mt-5">
+      <h3 class="text-2xl font-bold">Receive</h3>
+      <qrcode-vue :value="wallet.lightning_address" :size="200" level="L" />
+      <p>{{ wallet.lightning_address }}</p>
+    </section>
 
-    <button type="button" class="bg-light-blue text-lace text-xl p-3">
+    <button type="button" class="bg-light-blue text-lace text-xl p-3 mt-5">
       Send sats
     </button>
 
@@ -22,6 +27,7 @@
 
 <script setup lang="ts">
 import { View } from "../enum/view";
+import QrcodeVue from "qrcode.vue";
 
 defineProps(["wallet"]);
 defineEmits(["setView"]);
