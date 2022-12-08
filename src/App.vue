4c0f077f8ac4662132a4122e87fe510b4041b105
Fryheid <fryheid@protonmail.com>
2022-12-08 22:00:42 +0100

    Experimental dhv for better mobile display

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <body
-    class="bg-lace h-screen mh-screen text-dark-blue grid grid-rows-[min-content_1fr]"
+    class="bg-lace h-[100dvh] mh-screen text-dark-blue grid grid-rows-[min-content_1fr]"
   >
     <header class="bg-dark-blue container mx-auto max-w-screen-sm">
       <div class="mx-3">
