5b7c39bad588cf260c52f55c40bd6ffd0cacba9d
Fryheid <fryheid@protonmail.com>
2022-12-08 17:51:56 +0100

    Slightly larger text in 'Create a new wallet' button

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@
         class="h-full flex flex-col items-center justify-center relative mx-3 mt-3"
       >
         <span @click="view = View.Home" class="absolute top-0 left-0">← back</span>
-        <button type="button" class="w-full bg-light-blue text-lace p-3">
+        <button type="button" class="w-full bg-light-blue text-lace text-xl p-3">
           Create a new wallet
         </button>
         <button class="text-light-blue underline mt-3">
