7bcaa0f064e9f2c94b9fae03798316f51eea0dd7
Fryheid <fryheid@protonmail.com>
2022-12-10 00:50:46 +0100

    Dark blue border for avatar to match wallet name

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -1,6 +1,6 @@
 <template>
   <img
-    class="rounded-full h-[60px] min-w-[60px] border-2 border-light-blue"
+    class="rounded-full h-[60px] min-w-[60px] border-2 border-dark-blue"
     :src="`https://asats.io/anonsats/robohash/${walletName}`"
   />
 </template>
