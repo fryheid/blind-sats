4303ba2ed7ebe1e6e079533471c5480c88ca2f8e
Fryheid <fryheid@protonmail.com>
2022-12-09 19:59:31 +0100

    Imported modal package

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
+import { vfmPlugin } from 'vue-final-modal'
 import './assets/css/style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(vfmPlugin)
+.mount('#app')
