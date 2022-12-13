ae9d78ab9bf42746e48d5204e0b664e24cb1960f
Fryheid <fryheid@protonmail.com>
2022-12-13 21:26:13 +0100

    Loading animation for avatars

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -1,10 +1,15 @@
 <template>
   <img
     class="rounded-full h-[60px] min-w-[60px] border-2 border-dark-blue"
+    :class="{ 'animate-pulse': !loaded }"
     :src="`https://asats.io/anonsats/robohash/${walletName}`"
+    @load="loaded = true"
   />
 </template>
 
 <script setup lang="ts">
-defineProps(["walletName"])
+import { ref } from "vue";
+defineProps(["walletName"]);
+
+const loaded = ref(false);
 </script>
