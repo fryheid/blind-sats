bc3c508afdb1549a78bc5530568e3a171d7e970d
Fryheid <fryheid@protonmail.com>
2022-12-22 21:09:05 +0100

    Make classes important instead of repeating for dark

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -11,7 +11,7 @@
           <button
             type="button"
             @click="modals.backup = true"
-            class="btn bg-slate-400 dark:bg-slate-400 border-slate-400 dark:border-slate-400 text-slate-800"
+            class="btn !bg-slate-400 !border-slate-400 text-slate-800"
           >
             Backup
           </button>
@@ -46,7 +46,7 @@
         <button
           type="button"
           @click="$emit('forgetWallet', wallet.wallet_key)"
-          class="btn bg-red-300 dark:bg-red-300 border-red-300 dark:border-red-300 text-red-700 mr-3"
+          class="btn !bg-red-300 !border-red-300 text-red-700 mr-3"
         >
           Yes, forget
         </button>
