ab36ccf2e3e9529773af5f5343dc8f8fe46a810f
Fryheid <fryheid@protonmail.com>
2022-12-22 21:45:53 +0100

    Abstracted icon to component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,31 +6,8 @@
         @click="$emit('setView', View.AddWallet)"
         class="btn bg-orange border-orange dark:bg-harvest-gold dark:border-harvest-gold w-full md:w-1/2 flex justify-center items-baseline mt-3"
       >
-        <svg
-          class="w-8 pr-2 stroke-lace/90"
-          width="16"
-          height="16"
-          viewBox="0 0 16 16"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <line
-            x1="8"
-            y1="1"
-            x2="8"
-            y2="15"
-            stroke-width="2"
-            stroke-linecap="round"
-          />
-          <line
-            x1="15"
-            y1="8"
-            x2="1"
-            y2="8"
-            stroke-width="2"
-            stroke-linecap="round"
-          /></svg
-        >Add wallet
+        <PlusIcon class="w-8 pr-2 stroke-lace/90" />
+        Add wallet
       </button>
     </div>
   </div>
@@ -43,7 +20,9 @@
 
 <script setup lang="ts">
 import { View } from "../enum/view";
+import PlusIcon from "./PlusIcon.vue";
 import Wallets from "./Wallets.vue";
+
 defineProps(["wallets"]);
 defineEmits(["setView", "openWallet"]);
 </script>
---
+++ src/components/PlusIcon.vue
@@ -0,0 +1,27 @@
+<template>
+  <svg
+    class="w-8 pr-2 stroke-lace/90"
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <line
+      x1="8"
+      y1="1"
+      x2="8"
+      y2="15"
+      stroke-width="2"
+      stroke-linecap="round"
+    />
+    <line
+      x1="15"
+      y1="8"
+      x2="1"
+      y2="8"
+      stroke-width="2"
+      stroke-linecap="round"
+    />
+  </svg>
+</template>
