76de3a22be2bb172dff7d0d95e0d0945ac9a849b
Fryheid <fryheid@protonmail.com>
2022-12-22 21:51:20 +0100

    Small styling improvement

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,7 +6,7 @@
         @click="$emit('setView', View.AddWallet)"
         class="btn bg-orange border-orange dark:bg-harvest-gold dark:border-harvest-gold w-full md:w-1/2 flex justify-center items-baseline mt-3"
       >
-        <PlusIcon class="w-8 pr-2 stroke-lace/90" />
+        <PlusIcon class="mr-2 stroke-lace/90" />
         Add wallet
       </button>
     </div>
